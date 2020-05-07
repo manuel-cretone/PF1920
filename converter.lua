ace28036e066a27ab5ef5d8d129b3be79e48080e
097475 <39126261+097475@users.noreply.github.com>
2020-05-07 10:11:28 +0200

    Add files via upload

--- converter.lua
+++ converter.lua
@@ -1,20 +1,24 @@
+local binaryheap = require 'binaryheap'
+
+
+-- table.unpack == unpack
+
 -- debug variables
 visited = {
-  ["1|1|1"] = {"", 0 },
-  ["2|2|1"] = {"R", 1 },
-  ["1|2|2"] = {"D", -1 },
+  ["5|2|5"] = {"", 0 },
+  ["4|3|5"] = {"R", -1 },
+  ["3|3|4"] = {"U", -1 },
 }
 
 tab = {{"m", "m", "m", "m", "m", "m", "m"},
 {"m", 3, 1, 7, "m", "p","m"},
-{"m", "m", 3, "m", 8, 1, "m"},
+{"m", "m", 3, 0, 8, 1, "m"},
 {"m", 2, 1, "p", "m", "u", "m"},
 {"m", "i", 3, "m", "m", "m", "m"},
 {"m", 2, "m", 9, "m", "m", "m"}}
 
 t  = {life = 3, x = 1, y = 2}
 -------------------------------------------------
-
 
 -- input: two dimensional table
 -- output: none
@@ -75,16 +79,16 @@ function move_vector(move)
 
 
 -- input: state string final, hash table tree
--- output: string representing the steps taken in the path using DULR
+-- output: table representing the steps taken in the path using DULR and life change at each step
 function gen_path(final, tree)
   -- input: state string final, path string path
-  -- output: string representing the steps taken in the path using DULR
-  function _gen_path(final, path)
-    path = tree[final][1] .. path
+  -- output: table representing the steps taken in the path using DULR and life change at each step
+  function _gen_path(final, history)
     if tree[final][1] == "" then
-      return path
+      return history
     else
-      return _gen_path(invert_move(final, tree[final][1], tree[final][2]), path)
+      table.insert(history, 1, tree[final])
+      return _gen_path(invert_move(final, tree[final][1], tree[final][2]), history)
     end
   end
   -- input: state string, move used to get to state, life_chance applied after getting to state
@@ -95,30 +99,37 @@ function gen_path(final, tree)
     return encode(life - life_change, x - delta_x, y - delta_y)
   end
   
-  return _gen_path(final, "")
+  return _gen_path(final, {})
   end
 
--- input: path string composed by DULR characters, two dimensional table representing maze
--- output: None (Maze modified by side effect)
-function write_path(path, maze)
-  -- input: path string composed by DULR characters, x coordinate, y coordinate of current position in maze
-  -- output: None (Maze modified by side effect)
-  function _write_path(path, x, y)
-    local move = string.sub(path,1,1)
-    if move ~= "" then
-      local delta_x, delta_y = move_vector(move)
-      x = x + delta_x
-      y = y + delta_y
-      maze[y][x] = "*"
-      _write_path(string.sub(path, 2), x, y)
+-- input: table representing the steps taken in the path using DULR and life change at each step, two dimensional table representing maze, initial life
+-- output: modified maze, final life
+function write_path(history, maze, life)
+  -- input: table representing the steps taken in the path using DULR and life change at each step, x coordinate, y coordinate of current position in maze, initial life
+  -- output: final life
+  function _write_path(history, x, y, life)
+    local move = table.remove(history, 1)
+    local action = move[1]
+    local life_change = move[2]
+    local delta_x, delta_y = move_vector(action)
+    x = x + delta_x
+    y = y + delta_y
+    life = life + life_change
+    maze[y][x] = "*"
+    if #history > 0 then
+      return _write_path(history, x, y, life)
+    else
+      return life
     end
   end
   
   local x,y = find_initial(maze)
-  maze[y][x] = "*"
-  _write_path(path, x, y)
+  maze[y][x] = "*"  
+  return maze, _write_path(history, x, y, life)
 end
 
-print(gen_path("1|2|2", visited))
-write_path(gen_path("1|2|2", visited), tab)
-print_table(tab)
+--print_table(tab)
+--print_table(gen_path("3|3|4", visited))
+maze, life = write_path(gen_path("3|3|4", visited), tab, 5)
+print_table(maze)
+print(life)
---
+++ main.lua
@@ -0,0 +1,278 @@
+io.stdout:setvbuf("no")
+table.unpack = unpack
+require "converter"
+-- https://sheepolution.com/learn/book/18
+TILESET = "scifitiles-sheet.png"
+NOWALL = 21
+WALL = 5
+PIT = 18
+ENTRANCE = 4
+EXIT = 59
+width = 32
+height = 32
+
+function draw_arrowhead(x, y, orientation)
+  if orientation == "SOUTH" then
+    love.graphics.polygon('fill', x + width/2, y + height, x + 10, y + height - 10, x + width - 10, y + height - 10)
+  elseif orientation == "NORTH" then
+    love.graphics.polygon('fill', x + width/2, y, x + 10, y + 10, x + width - 10, y + 10)
+  elseif orientation == "EAST" then
+    love.graphics.polygon('fill', x + width, y + height/2, x + width - 10, y + 10, x + width - 10, y + height - 10)
+  elseif orientation == "WEST" then
+    love.graphics.polygon('fill', x, y + height/2, x + 10, y + 10, x + 10, y + height - 10)
+  end
+end
+
+function draw_line(x, y, orientation, offset)
+  offset = offset or 0
+  if orientation == "SOUTH" then
+    love.graphics.line(x + width/2, y, x+ width/2, y + height - offset)
+  elseif orientation == "NORTH" then
+    love.graphics.line(x + width/2, y + offset, x+ width/2, y + height)
+  elseif orientation == "EAST" then
+    love.graphics.line(x, y + height/2, x+ width - offset, y + height/2)
+  elseif orientation == "WEST" then
+    love.graphics.line(x + offset, y + height/2, x+ width, y + height/2)
+  elseif orientation == "SOUTHEAST" then
+    love.graphics.line(x + width/2, y, x+ width/2, y + height/2)
+    love.graphics.line(x + width/2 - love.graphics.getLineWidth() / 2, y + height/2, x+ width - offset, y + height/2)
+  elseif orientation == "SOUTHWEST" then
+    love.graphics.line(x + width/2, y, x+ width/2, y + height/2)
+    love.graphics.line(x + width/2 + love.graphics.getLineWidth() / 2, y + height/2, x + offset, y + height/2)
+  elseif orientation == "NORTHEAST" then
+    love.graphics.line(x + width/2, y + height, x+ width/2, y + height/2) 
+    love.graphics.line(x + width/2 - love.graphics.getLineWidth() / 2, y + height/2, x+ width - offset, y + height/2)
+  elseif orientation == "NORTHWEST" then
+    love.graphics.line(x + width/2, y + height, x+ width/2, y + height/2)
+    love.graphics.line(x + width/2 + love.graphics.getLineWidth() / 2, y + height/2, x + offset, y + height/2)
+  end 
+end
+
+
+function draw_arrow(x, y, orientation)
+  if orientation == "SOUTH" then
+    draw_line(x, y, orientation, 10)
+    draw_arrowhead(x, y, "SOUTH")
+  elseif orientation == "NORTH" then
+    draw_line(x, y, orientation, 10)
+    draw_arrowhead(x, y, "NORTH")
+  elseif orientation == "EAST" or orientation == "SOUTHEAST" or orientation == "NORTHEAST"then
+    draw_line(x, y, orientation, 10)
+    draw_arrowhead(x, y, "EAST")
+  elseif orientation == "WEST" or orientation == "SOUTHWEST" or orientation == "NORTHWEST" then
+    draw_line(x, y, orientation, 10)
+    draw_arrowhead(x, y, "WEST")
+  end  
+end
+
+
+function draw_origin(x, y, orientation, with_arrow)
+  love.graphics.circle("fill", x + width/2, y + height/2, 5)
+  
+  if(with_arrow) then
+    offset = 10
+    draw_arrowhead(x, y, orientation)
+  else
+    offset = 0
+  end
+  
+  if orientation == "SOUTH" then
+    love.graphics.line(x + width/2, y + height/2, x+ width/2, y + height - offset)
+  elseif orientation == "NORTH" then
+    love.graphics.line(x + width/2, y + height/2, x+ width/2, y + offset)
+  elseif orientation == "EAST" then
+    love.graphics.line(x + width/2, y + height/2, x+ width - offset, y + height/2)
+  elseif orientation == "WEST" then
+    love.graphics.line(x + width/2, y + height/2, x+ offset, y + height/2)
+  end  
+
+end
+
+
+--punta a
+function draw_destination(x, y, orientation)
+  draw_line(x, y, orientation, width-10)
+  if orientation == "SOUTH" then
+    love.graphics.polygon('fill', x + width/2, y + height/2, x + 10, y + height/2 - 10, x + width - 10, y + height/2 - 10)
+  elseif orientation == "NORTH" then
+    love.graphics.polygon('fill', x + width/2, y + height/2, x + 10, y + height/2 + 10, x + width - 10, y + height/2 + 10)
+  elseif orientation == "EAST" then
+    love.graphics.polygon('fill', x + width/2, y + height/2, x + width/2 - 10, y + 10, x + width/2 - 10, y + height - 10)
+  elseif orientation == "WEST" then
+    love.graphics.polygon('fill', x + width/2, y + height/2, x + width/2 + 10, y + 10, x + width/2 + 10, y + height - 10)
+  end
+end
+
+
+function draw_moves(history, current_move)
+  local i = 0
+  local current_x, current_y = find_initial(maze)
+  while i < current_move do
+    i = i + 1
+    if i == 1 then
+      move = history[i][1]
+      delta_x, delta_y = move_vector(move)
+      if delta_x == 1 then
+        draw_origin(current_x*width, current_y*height, "EAST", false)
+      elseif delta_x == -1 then
+        draw_origin(current_x*width, current_y*height, "WEST", false)
+      elseif delta_y == 1 then
+        draw_origin(current_x*width, current_y*height, "SOUTH", false)
+      elseif delta_y == -1 then
+        draw_origin(current_x*width, current_y*height, "NORTH", false)
+      end
+      current_x, current_y = current_x + delta_x, current_y + delta_y
+      prev_delta_x, prev_delta_y = delta_x, delta_y
+    elseif i <= current_move then
+      move = history[i][1]
+      delta_x, delta_y = move_vector(move)
+      if prev_delta_x ~= 0 and delta_x ~= 0 then
+        draw_line(current_x*width, current_y*height, "EAST")
+      elseif prev_delta_y ~= 0 and delta_y ~= 0 then
+        draw_line(current_x*width, current_y*height, "SOUTH")
+      elseif (prev_delta_y == 1 and delta_x == -1) or (prev_delta_x == 1 and delta_y == -1) then
+        draw_line(current_x*width, current_y*height, "SOUTHWEST")
+      elseif (prev_delta_y == 1 and delta_x == 1) or (prev_delta_x == -1 and delta_y == -1) then
+        draw_line(current_x*width, current_y*height, "SOUTHEAST")
+      elseif (prev_delta_y == -1 and delta_x == -1) or (prev_delta_x == 1 and delta_y == 1) then
+        draw_line(current_x*width, current_y*height, "NORTHWEST")
+      elseif (prev_delta_y == -1 and delta_x == 1) or (prev_delta_x == -1 and delta_y == 1) then
+        draw_line(current_x*width, current_y*height, "NORTHEAST")
+      end
+      current_x, current_y = current_x + delta_x, current_y + delta_y
+      prev_delta_x, prev_delta_y = delta_x, delta_y
+    end
+    if i == current_move then
+      move = history[i][1]
+      delta_x, delta_y = move_vector(move)
+      if delta_x == 1 then
+        draw_destination(current_x*width, current_y*height, "EAST")
+      elseif delta_x == -1 then
+        draw_destination(current_x*width, current_y*height, "WEST")
+      elseif delta_y == 1 then
+        draw_destination(current_x*width, current_y*height, "SOUTH")
+      elseif delta_y == -1 then
+        draw_destination(current_x*width, current_y*height, "NORTH")
+      end
+    end
+  end
+
+
+
+    
+  
+--  local i = 2
+--  while i < current_move do
+--    i = i + 1
+--    draw_move(x, y, history[i][1])
+--  end
+end
+
+function generate_tilemap(maze)
+  local tilemap = {}
+  
+  
+  for i,row in ipairs(maze) do
+    table.insert(tilemap, {})
+    for j,tile in ipairs(row) do
+      if tile == "p" then
+        table.insert(tilemap[i], PIT)
+      elseif tile == "u" then
+        table.insert(tilemap[i], EXIT)
+      elseif tile == "i" then
+        table.insert(tilemap[i], ENTRANCE)
+      elseif tile ~= "m" then
+        table.insert(tilemap[i], NOWALL)
+      else
+        table.insert(tilemap[i], WALL)
+      end
+    end
+  end
+
+  return tilemap
+  
+end
+
+
+
+
+function love.load()
+    --tick = require "tick"
+    life = 5
+    maze = {{"m", "m", "m", "m", "m", "m", "m"},
+            {"m", 3, 1, 7, "m", "p","m"},
+            {"m", "m", 3, 0, 8, 1, "m"},
+            {"m", 2, 1, "p", "m", "u", "m"},
+            {"m", "i", 3, "m", "m", "m", "m"},
+            {"m", 2, "m", 9, "m", "m", "m"}}
+    history = {{"U", 2},{"R", 1},{"U", 3},{"R", 0},{"R", -4},{"R", 1},{"D", 0}}
+    current_move = 0
+    index = 0
+    
+    
+    
+    
+    image = love.graphics.newImage(TILESET)
+    local image_width = image:getWidth()
+    local image_height = image:getHeight()
+
+    quads = {}
+    for i=0,5 do
+      for j=0,13 do
+        --The only reason this code is split up in multiple lines
+        --is so that it fits the page
+        table.insert(quads, love.graphics.newQuad(j * width, i * height, width, height, image_width, image_height))
+      end
+    end
+    
+    
+  tilemap = generate_tilemap(maze)
+    
+  love.graphics.setFont(love.graphics.newFont(12))
+  love.graphics.setLineWidth(5)
+  love.graphics.setLineStyle("rough")
+  love.window.maximize()
+end
+
+function love.update(dt)
+  --tick.update(dt)
+  while index < current_move do
+    index = index + 1
+    life = life + history[index][2]
+  end
+end
+
+function love.draw()
+  
+  love.graphics.print("Life: " .. life, width, height/2)
+  
+  love.graphics.print("Keys: [SPACE] -> Execute one step \t [ENTER] -> Execute full path \t [BACKSPACE] -> Backtrack one step \t [DELETE] -> Backtrack all steps", width*3, height/2)
+  
+  for i,row in ipairs(tilemap) do
+    for j,tile in ipairs(row) do
+      --Draw the image with the correct quad
+      love.graphics.draw(image, quads[tile], j * width, i * height)
+      if type(maze[i][j]) == "number" or maze[i][j] == "f" then
+          love.graphics.setColor(0,0,0,1)
+          love.graphics.print(maze[i][j], j * width, i * height)
+          love.graphics.setColor(1,1,1,1)
+      end
+    end
+  end
+  
+  draw_moves(history, current_move)
+  
+end
+
+
+function love.keypressed(key, scancode, isrepeat)
+   if key == "space" and current_move < #history then
+      current_move = current_move + 1
+    elseif key == "return" then
+      current_move = #history
+    elseif key == "backspace" then
+      current_move = current_move - 1
+    elseif key == "delete" then
+      current_move = 0
+   end
+end
